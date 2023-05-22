a610d36a649367177055adac2eb2045f2d9b12b1
Nathan Farrel <nathanfarrel1128@gmail.com>
2023-05-22 20:31:03 +0700

    second change

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ const roll = document.querySelector(".btn--roll");
 const dice = document.querySelector('.dice');
 const hold = document.querySelector(".btn--hold");
 const reset = document.querySelector(".btn--new")
+const current0 = document.getElementById("current--0");
+const current1 = document.getElementById("current--1");
+
 
 function changePlayer() {
   if (player0.classList.contains("player--active")) { 
@@ -20,7 +23,7 @@ function changePlayer() {
 };
 
 function unLucky() {
-  player0.classList.contains("player--active") ? score0.textContent = 0 : score1.textContent = 0;
+  player0.classList.contains("player--active") ? current0.textContent = 0 : current1.textContent = 0;
   changePlayer();
 };
 
@@ -29,7 +32,6 @@ function rollDice() {
   dice.classList.remove("hidden");
   dice.src = `dice-${ran}.png`;
   ran === 1 ? unLucky() : addScore(ran);
-  checkScore();
 };
   
 function checkScore() {
@@ -43,9 +45,9 @@ function checkScore() {
 
 function addScore(ran) {
   if (player0.classList.contains("player--active")) {
-    score0.textContent = Number(score0.textContent) + ran;
+    current0.textContent = Number(current0.textContent) + ran;
   } else {
-    score1.textContent = Number(score1.textContent) + ran;
+    current1.textContent = Number(current1.textContent) + ran;
   }
 };
 
@@ -56,6 +58,18 @@ function resets() {
   !player0.classList.contains("player--active") && changePlayer(); 
 };
 
+function holdScore() {
+  if (player0.classList.contains("player--active")) {
+    score0.textContent = Number(score0.textContent) + Number(current0.textContent);
+    current0.textContent = 0;
+  } else {
+    score1.textContent = Number(score1.textContent) + Number(current1.textContent);
+    current1.textContent = 0;
+  }
+  checkScore();
+  changePlayer();
+};
+
 roll.addEventListener("click", rollDice);
-hold.addEventListener("click", changePlayer);
+hold.addEventListener("click", holdScore);
 reset.addEventListener("click", resets);
